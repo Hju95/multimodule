92679e683d418c2ced2af6b89f7ca49ee5391ab5
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-12-11 21:17:42 +0900

    test

---
+++ module-api/src/main/java/dev/be/moduleapi/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package dev.be.moduleapi.exceptionhandler;
+
+import dev.be.moduleapi.exception.CustomException;
+import dev.be.moduleapi.response.CommonResponse;
+import dev.be.modulecommon.enums.CodeEnum;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public CommonResponse handlerCustomException(CustomException e) {
+        return CommonResponse.builder()
+                .returnCode(e.getReturnCode())
+                .returnMessage(e.getReturnMessage())
+                .build();
+    }
+
+    //아무리 꼼꼼하게 코딩을 했더라도 놓치는 에러
+    @ExceptionHandler(Exception.class)
+    public CommonResponse handlerException(Exception e) {
+        return CommonResponse.builder()
+                .returnCode(CodeEnum.UNKNOWN_ERROR.getCode())
+                .returnMessage(CodeEnum.UNKNOWN_ERROR.getMessage())
+                .build();
+    }
+}
---
+++ module-api/src/main/java/dev/be/moduleapi/response/CommonResponse.java
@@ -0,0 +1,37 @@
+package dev.be.moduleapi.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@JsonInclude(Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> {
+    private String returnCode;
+    private String returnMessage;
+    private T info;
+
+    public CommonResponse(CodeEnum codeEnum) {
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+    }
+
+    // info 만 줄테니 너가 세팅을 알아서 해줘.. Info 값이 넘어온거라면 성공으로 간주
+    public CommonResponse(T info) {
+        setReturnCode(CodeEnum.SUCCESS.getCode());
+        setReturnMessage(CodeEnum.SUCCESS.getMessage());
+        setInfo(info);
+    }
+
+    // 둘 다 보낼 때
+    public CommonResponse(CodeEnum codeEnum, T info) {
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+        setInfo(info);
+    }
+}
